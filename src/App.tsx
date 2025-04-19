523b413041ad66b3ec2f70b3f76e4386363c0fea
Michael Redruello <84834705+michaelredruello@users.noreply.github.com>
2025-04-19 05:03:56 +0200

    css fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
